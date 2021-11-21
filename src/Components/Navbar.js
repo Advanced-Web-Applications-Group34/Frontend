d682010d874348da55a2851e4caa5547c2343a68
Jolan <jolan.callens@gmail.com>
2021-11-21 18:31:48 +0200

    style navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import styles from './navbar.module.css'
 
 export default function NavBar() {
     return (
         <div>
-            <nav className="navbar">
+            <nav className={styles.navbar}>
 
                 {/* App title */}
-                <h1>AWA</h1>
+                <h1 className={styles.title}>AWA</h1>
 
                 {/* Pages */}
-                <div className="pages">
+                <div className={styles.pages}>
                     <Link to="/">Home</Link>
                     <Link to="/restaurants">Restaurants</Link>
                 </div>
 
                 {/* Cart and user personal page */}
-                <div className="userPart">
+                <div className={styles.userPart}>
                     <Link to="/login">Log in</Link>
                     <p>Cart</p>
                 </div>
