515e59f0c9d880169cd9de699680f9d1440b8f7c
Jolan <jolan.callens@gmail.com>
2021-11-21 20:35:22 +0200

    style searchView page (restaurants and categories elements)

--- src/Components/SearchView.js
+++ src/Components/SearchView.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import styles from '../Styles/homePageElements.module.css'
+import styles from '../Styles/searchView.module.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBiking } from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 export default function SearchView(props) {
 
@@ -15,24 +19,49 @@ export default function SearchView(props) {
 
                 <div className={styles.elementsGrid}>
                     {restaurants.map(restaurant => (
-                        <div className="restaurant-preview" key={restaurant.id} >
+                        <div className={styles.elementPreview} key={restaurant.id} >
                             <img src={`/images/restaurants/${restaurant.image}`} alt={restaurant.name} />
-                            <h3>{restaurant.name}</h3>
-                            <h4>{restaurant.categories.map(category => (
-                                <span>{category}</span>
-                            ))}</h4>
+
+                            <div className={styles.bottomElement}>
+
+                                <h3>{restaurant.name}</h3>
+                                <h4>{restaurant.categories.map(category => (
+                                    <span>{category}</span>
+                                ))}</h4>
+
+                                <div className={styles.informations}>
+                                    <div className={styles.deliveryPrice}>
+                                        <FontAwesomeIcon icon={faBiking} />
+                                        <p>€1.90</p>
+                                    </div>
+                                    <div className={styles.priceRange}>
+                                        <p>{restaurant.priceRange}</p>
+                                    </div>
+                                    <div className={styles.rate}>
+                                        <FontAwesomeIcon icon={faStar} />
+                                        <p>{restaurant.rate}/5</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                                
                         </div>
                     ))}
                 </div>
 
                 <h2>Categories</h2>
 
-                {categories.map(category => (
-                    <div className="category-preview" key={category.id} >
-                        <img src={`/images/restaurants/${category.image}`} alt={category.name} />
-                        <h3>{category.name}</h3>
-                    </div>
-                ))}
+                <div className={styles.elementsGrid}>
+                    {categories.map(category => (
+                        <div className={styles.elementPreview} key={category.id} >
+                            <img src={`/images/restaurants/${category.image}`} alt={category.name} />
+
+                            <div className={styles.bottomElement}>
+                                <h3>{category.name}</h3>
+                            </div>
+                        </div>
+                    ))}
+                </div>
 
 
             </div>
