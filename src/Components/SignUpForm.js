670aa4aafa53b3350611f4fdc604e50879de70c9
TramVu <71011294+TramVu@users.noreply.github.com>
2021-11-26 20:33:36 +0200

    create new form

---
+++ src/Components/SignUpForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function SignUpForm() {
+    return (
+        <div className = "card">
+            <div>
+                <p>SIGN UP</p>
+            </div>
+            <div className="actions">
+                <button className="btn1">Create an user account</button>
+                <button className="btn1">Create a restaurant account</button>
+            </div>
+        </div>
+    )
+}
