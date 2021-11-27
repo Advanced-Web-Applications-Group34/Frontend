aff99952c206adc5255f2ab6fa767fe5022a021f
TramVu <71011294+TramVu@users.noreply.github.com>
2021-11-27 15:36:35 +0200

    initial stages

---
+++ src/Components/UserSignupSuccess.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../Styles/UserSignupSuccess.module.css';
+
+export default function UserSignupSuccess() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.contentWrapper}>
+                <h1 className={styles.success}>Account created!</h1>
+            </div>
+            
+        </div>
+    )
+}
