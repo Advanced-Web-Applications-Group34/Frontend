81670a98dbab3648595dfcc39ec6b6946858a378
TramVu <71011294+TramVu@users.noreply.github.com>
2021-12-05 13:10:01 +0200

    update file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import {BrowserRouter, Routes,Route} from "react-router-dom";
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Restaurants from './Components/Restaurants';
-import SignInForm from "./Components/SignInForm";
-import SignUpForm from "./Components/SignUpForm";
-//import UserForm from './Components/UserForm';
-import UserSignupForm from "./Components/UserSignupForm";
-//import UserSignupSuccess from './Components/UserSignupSuccess';
+// import SignInForm from "./Components/SignInForm";
+// import SignUpForm from "./Components/SignUpForm";
+// import UserSignupForm from "./Components/UserSignupForm";
+import ResSignupForm from "./Components/ResSignupForm";
 import Cart from "./Components/Cart";
 
 function App() {
@@ -21,10 +20,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="restaurants" element={<Restaurants/>} />
-            <Route path="login" element={<SignInForm/>} />
-            <Route path="/login/SignUpForm" element={<SignUpForm/>} />
-            <Route path="/login/SignUpForm/UserSignupForm" element={<UserSignupForm/>} />
-
+            <Route path="login" element={<ResSignupForm/>} />
+            {/* <Route path="/login/SignUpForm" element={<SignUpForm/>} />
+            <Route path="/login/SignUpForm/UserSignupForm" element={<UserSignupForm/>} /> */}
             <Route path="cart" element={<Cart/>} />
           </Routes>
         </div>
