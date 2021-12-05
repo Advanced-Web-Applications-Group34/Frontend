a10bd1d50d6a200d93807b410645e1680c29c7fe
TramVu <71011294+TramVu@users.noreply.github.com>
2021-12-05 18:10:35 +0200

    editing

--- src/Components/RestaurantForms/AdminView.js
+++ src/Components/RestaurantForms/AdminView.js
@@ -1,45 +1,99 @@
 import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
+import styles from '../../Styles/RestaurantForms/AdminView.module.css';
 
-export default function AdminView(props) {
+export default function AdminView() {
 
     const [newItemName, setNewItemName] = useState("");
-    const [newItemAuthor, setNewItemAuthor] = useState("");
+    const [newItemCatergory, setNewItemCatergory] = useState({
+        value: 'Burger'
+    });
+    const [newItemDescription, setNewItemDescription] = useState("")
     const [newItemPrice, setNewItemPrice] = useState("");
+    const [newItemImage, setNewItemImage] = useState("")
+
+    const [allItem, setAllItem] = useState ([]);
 
     const addNewItem = () => {
-        props.addNewItem(newItemName, newItemAuthor, newItemPrice);
+        const newItem = 
+        {
+            name: newItemName, 
+            category: newItemCatergory,
+            description: newItemDescription,
+            price:newItemPrice,
+            image: newItemImage,
+        };
+        setAllItem([...allItem, newItem]);
     }
 
-    const onDeleteItemClick = (itemId) => {
-        console.log("clicked delete for item id " + itemId);
-        props.deleteItem(itemId);
-    }
+    // const onDeleteItemClick = (itemId) => {
+    //     console.log("clicked delete for item id " + itemId);
+    //     props.deleteItem(itemId);
+    // }
 
     return (
-        <div>
-        <div>
-            <h1>Add new item</h1>
-            <div>
-                Name <input type="text" onChange={ (event) => setNewItemName(event.target.value) } />
-            </div>
-            <div>
-                Author <input type="text" onChange={ (event) => setNewItemAuthor(event.target.value) } />
-            </div>
-            <div>
-                Price <input type="text" onChange={ (event) => setNewItemPrice(event.target.value) } />
-            </div>
-            <button onClick={ addNewItem }>Add Item</button>
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <label>Add new item</label>
+                <div>Catergory
+                    <select className={styles.resType} value={newItemCatergory} onChange={(e) => setNewItemCatergory(e.target.value)}>
+                        <option value="Burger">Buffet</option>
+                        <option value="Sushi">Fast-food</option>
+                        <option value="Pizza">Fast-casual</option>
+                        <option value="Mexican">Casual dining</option>
+                        <option value="Kebap">Fine dining</option>
+                        <option value="Asian">Fine dining</option>
+                        <option value="Finnish">Fine dining</option>
+                    </select>
 
+                </div>
+                <div>
+                    Name: <input type="text" 
+                                value={newItemName}
+                                onChange={ (e) => setNewItemName(e.target.value) } />
+                </div>
+                <div>
+                    Description: <input type="text" 
+                                        value={newItemDescription}
+                                        onChange={ (e) => setNewItemDescription(e.target.value) } />
+                </div>
+                <div>
+                    Price: € <input type="text" 
+                                value={newItemPrice} 
+                                onChange={ (e) => setNewItemPrice(e.target.value) } />
+                </div>
+                <div>
+                    Choose file: <input type ='image' 
+                                        value ={newItemImage}
+                                        onchange={ (e) => setNewItemImage(e.target.value) } />
+                </div>
+                <button className ={styles.addingBtn} onClick={ addNewItem }>Add Item</button>
             </div>
+
+            <div>
+                <h1>List of items</h1>
+                { 
+                    allItem.map((curElem) =>{
+                        return (
+                            <div>
+                                <p>{curElem.newItemCatergory}</p>
+                                <p>{curElem.newItemName}</p>
+                                <p>{curElem.newItemDescription}</p>
+                                <p>{curElem.newItemPrice}</p>
+                                <p>{curElem.newItemImage}</p>
+                                {/* <button onClick={() => onDeleteItemClick(item.id)}>X</button> {item.name}, {item.author}, {item.price} */}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+            <div>
             <Link to="./Menu">
-                <button>Disable Admin Mode</button>
+                <button className={styles.disableBtn}>Disable Admin Mode</button>
             </Link>
-            <h1>List of items</h1>
-            { props.items.map((item, index) =>
-            <div key={index}>
-                <button onClick={() => onDeleteItemClick(item.id)}>X</button> {item.name}, {item.author}, {item.price}
-            </div>)}
+            </div>
+
         </div>
     )
 }
